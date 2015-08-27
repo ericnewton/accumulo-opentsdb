f12426cc1f448b1adae53d6a9d9dae8e9427c02e
Eric Newton <eric.newton@gmail.com>
2015-08-27 14:45:43 -0400

    create the destination diretory for installing over in opentsdb

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 mvn package
 jar=$(cd target ; ls *.jar)
+mkdir -p ../opentsdb/third_party/accumulo-opentsdb
 md5sum target/$jar > ../opentsdb/third_party/accumulo-opentsdb/$jar.md5
 cp target/$jar ../opentsdb/third_party/accumulo-opentsdb
